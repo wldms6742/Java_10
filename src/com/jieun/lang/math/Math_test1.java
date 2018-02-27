805b343fd29d8a403229f1a029cdb2ae07979bbe
wldms6742 <36650354+wldms6742@users.noreply.github.com>
2018-02-27 15:00:51 +0900

    0227-1

--- src/com/jieun/lang/math/Math_test1.java
+++ src/com/jieun/lang/math/Math_test1.java
@@ -1,11 +1,14 @@
 package com.jieun.lang.math;
 
+import java.util.Date;
+
 public class Math_test1 {
 
 	public static void main(String[] args) {
 		double d =Math.random();
 		
 		System.out.println((int)(d*10));
+		Date date = new Date(2012, 11, 1);//쓰지말것을 권장
 		
 	}
 
