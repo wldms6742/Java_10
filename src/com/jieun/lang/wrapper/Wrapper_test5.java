59c762b4a23949f3c2a0970a083ec5078700136a
wldms6742 <36650354+wldms6742@users.noreply.github.com>
2018-03-02 14:24:43 +0900

    teachercode

--- src/com/jieun/lang/wrapper/Wrapper_test5.java
+++ src/com/jieun/lang/wrapper/Wrapper_test5.java
@@ -33,6 +33,37 @@ public class Wrapper_test5 {
 		//나머지를 11-나머지 =>result
 		//result== 마지막숫자(체크값) 같으면 올바른 주민번호 아니면 틀린주민번호 
 		//결과값이 두자리라면 %10 해서 나머지값 -> 해당 나머지값과 체크값이 같은지 비교
+		char ch = number.charAt(0);
+		int num = Integer.parseInt(number.substring(0,1));
+		//num = Integer.parseInt(String.valueOf(ch));
+		//num = Integer.parseInt(Character.toString(ch));
+		//num = Integer.parseInt(ch+"");//아무런 값이 없는 문자열을 더해주면 문자열이됨
+		
+		int total =0;
+		int k=2;
+		for(int i=0;i<number.length()-1;i++) {
+			if(i==6) {
+				continue;//밑에를 하지않고 조건문으로 올라감
+			}
+			total = total + Integer.parseInt(number.charAt(i)+"")*k;
+			k++;
+			if(k==10) {
+				k=2;
+			}
+			
+		}
+		total = total/11;
+		total = 11-total;
+		if(total>=10) {
+			total = total%10;
+		}
+		int b =Integer.parseInt(number.charAt(number.length()-1)+"");
+		if(b==total) {
+			System.out.println("올바른 주민번호");
+		}else
+		{
+			System.out.println("틀린 주민번호");
+		}
 		
 		}
 
