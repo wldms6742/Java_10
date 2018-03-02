3d539e6498b27db9b9103278ba3eb060c0641e76
wldms6742 <36650354+wldms6742@users.noreply.github.com>
2018-03-02 14:07:54 +0900

    03-02

--- src/com/jieun/lang/wrapper/Wrapper_test5.java
+++ src/com/jieun/lang/wrapper/Wrapper_test5.java
@@ -33,34 +33,6 @@ public class Wrapper_test5 {
 		//나머지를 11-나머지 =>result
 		//result== 마지막숫자(체크값) 같으면 올바른 주민번호 아니면 틀린주민번호 
 		//결과값이 두자리라면 %10 해서 나머지값 -> 해당 나머지값과 체크값이 같은지 비교
-		int total = 0;
-		int result = 0;
-		String a1 = number.substring(0, 5);
-		String a2 = number.substring(7);
-		String numbers = a1.concat(a2);
-		System.out.println(numbers);
-		int j = 2;
-		for(int i=0;i<numbers.length()-1;i++) {
-				int num =Integer.parseInt(numbers.substring(i, i+1));
-				total =total + (num*j);
-				j=j+1;
-				if(j==9) {
-					j=2;
-				}
-			}
-		
-		System.out.println(total);
-		result = total % 11;
-		result = 11-result;
-		
-		if(result>9) {
-			result= result%10;
-		}
-		if(result==Integer.parseInt(numbers.substring(11))) {
-			System.out.println("올바른주민번호");
-		}else {
-			System.out.println("잘못된 주민번호");
-		}
 		
 		}
 
